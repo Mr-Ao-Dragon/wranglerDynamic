1f515bda08520649868d502cf6b772303a7835d7
敖律风 <x123456789fy@outlook.com>
2024-12-21 22:21:01 +0800

    [wip]: sed action

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+// let core = require('@actions/core')
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as process from 'node:process'
+import * as fs from 'node:fs'
 
 /**
  * The main function for the action.
@@ -7,14 +9,35 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    let targetFile: string
+    const templateType = core.getInput('template')
+    if (
+      !templateType.endsWith('toml.template') ||
+      !templateType.endsWith('json.template')
+    ) {
+      core.setFailed('Invalid template type')
+      return
+    }
+    if (templateType.endsWith('toml.template')) {
+      targetFile = __dirname + '/' + 'wrangler.toml'
+    } else if (templateType.endsWith('json.template')) {
+      targetFile = __dirname + '/' + 'wrangler.json'
+    }
+    const envVars: Record<string, string> = {}
+    for (const key in process.env) {
+      if (key.startsWith('__VARIABLE__') || key.startsWith('__SECRET__')) {
+        envVars[key] = process.env[key] || ''
+      }
+    }
+    core.notice('targets searched')
+    // read template file
+    let template: string
+    template = fs.readFileSync(__dirname + '/' + templateType, 'utf-8')
+    for (const key in template.search(/__VARIABLE__/g)) {
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    }
 
     // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
     // Set outputs for other workflow steps to use
