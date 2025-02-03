983c8a357b2588a1a47c7087788bbd365d35d0a1
一德 <1028647581@qq.com>
2025-02-03 19:51:45 +0800

    chang:按照需求更改了替换函数部分，解决合并冲突

--- demoFile/demo.ts
+++ demoFile/demo.ts
@@ -1,12 +1,6 @@
 import * as fs from 'node:fs'
-<<<<<<< HEAD
 
 const templateType = 'wrangler.toml.template';
-=======
-import * as yaml from 'js-yaml';
-
-const templateType = 'ci.yml';
->>>>>>> aa7120d76a9380ca4bf2028ad322181da018062b
 let template: string = fs.readFileSync(__dirname + '/' + templateType, 'utf-8')
 
 const regexVARIABLE =/\${{__VARIABLE__([A-Za-z0-9_]+)}}/g;
@@ -33,11 +27,7 @@ while ((matches = regexSECRET.exec(template)) !== null){
 /**
  * 执行搜索
  * 搜索出环境变量里有且模板里能寻找到对应值的
-<<<<<<< HEAD
  * @returns {string[]} 被调用后返回相同地对应值
-=======
- * @returns {string[]} 被调用后返回相同的对应值
->>>>>>> aa7120d76a9380ca4bf2028ad322181da018062b
  */
 export function search(variableWord: string[], lieuWord: string[]): string[]{
     // 创建数据存储列表，存储相同的关键字
@@ -59,17 +49,10 @@ export function search(variableWord: string[], lieuWord: string[]): string[]{
  * 执行替换并输出替换文本
  * @returns {undefined} 被调用后返回
  */
-<<<<<<< HEAD
 export function replaceText(replaceWord: { [type: string]: string },targetContext: string[]): string{
     for (const key in targetContext){
         // 替换模版内关键字为需求字段
         template = template.replace('${{'+targetContext[key]+'}}',replaceWord[targetContext[key]])
-=======
-export function replaceText(replaceWord: { [type: string]: string },lieuWord: string[]): string{
-    for (const key in lieuWord){
-        // 替换模版内关键字为需求字段
-        template = template.replace('${{'+lieuWord[key]+'}}',replaceWord[lieuWord[key]])
->>>>>>> aa7120d76a9380ca4bf2028ad322181da018062b
         console.log(replaceWord)
     }
     return template
@@ -78,7 +61,6 @@ export function replaceText(replaceWord: { [type: string]: string },lieuWord: st
 
 
 /**
-<<<<<<< HEAD
  * 调用directReplacement需要传入两个参数
  * 第一个为替换的模版
  * 第二个为替换的模版内关键字对应的需求字段
@@ -97,33 +79,3 @@ export function directReplacement(templateFile: string, replacements: { [key: st
     // 将处理后的内容写回文件
     fs.writeFileSync(templateFile, templateContent, 'utf8');
 }
-=======
- * 将转换好的YML配置文件转为TOML
- * @returns {undefined} 被调用后返回
- */
-export function conversionTOML(templateFile: string): undefined{
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const parsedData = yaml.load(templateFile) as any;
-
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const toml = require('@iarna/toml');
-    let tomlString: string = toml.stringify(parsedData);
-    tomlString = tomlString.replace(/_/g, '');
-    // 使用 fs.writeFileSync 同步写入文件
-    fs.writeFileSync(__dirname + '/' + 'ci.toml', tomlString, 'utf8');
-}
-
-// 模拟定义环境变量
-// const templateData: string[] = ['__VARIABLE__CMD','SECRET__PASSWORLD']
-// // 模拟定义关键字字段
-// const replaceData: { [type: string]: string } = {'__VARIABLE__CMD':'./run.sh','SECRET__PASSWORLD':'114514'}
-
-// // 搜索相同
-// const valueList: string[] = search(variables,templateData)
-
-// // 替换文本
-// const conversionText: string = replaceText(replaceData,valueList)
-
-// // 转换为TOML
-// conversionTOML(conversionText)
->>>>>>> aa7120d76a9380ca4bf2028ad322181da018062b
